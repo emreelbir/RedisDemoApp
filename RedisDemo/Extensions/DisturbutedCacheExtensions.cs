2a5a49b7d0597f94755901cff4cfc0f0ab8a4b59
Emre Elbir <59312457+emreelbir@users.noreply.github.com>
2020-12-11 16:16:21 +0100

    Add project files.

---
+++ RedisDemo/Extensions/DisturbutedCacheExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Extensions
+{
+    public static class DisturbutedCacheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absolutedExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            options.AbsoluteExpirationRelativeToNow = absolutedExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache,string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if(jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
